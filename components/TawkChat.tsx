0d6024324ca3dc28f2a5542edbdf552b79e2dbdb
solid-web <ozoaniphilip49@gmail.com>
2026-06-11 01:03:53 +0100

    fix: update tawk.to script ID

--- components/TawkChat.tsx
+++ components/TawkChat.tsx
@@ -6,7 +6,7 @@ export default function TawkChat() {
     const s1 = document.createElement('script')
     const s0 = document.getElementsByTagName('script')[0]
     s1.async = true
-    s1.src = 'https://embed.tawk.to/6a2603002d41c91c2b8503e5/default'
+    s1.src = 'https://embed.tawk.to/6a29e6978705f01c35099f82/default'
     s1.charset = 'UTF-8'
     s1.setAttribute('crossorigin', '*')
     s0.parentNode?.insertBefore(s1, s0)
