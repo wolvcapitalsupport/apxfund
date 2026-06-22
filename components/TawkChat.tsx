f19637bb4271a579c49c2f5220fab479774aca0a
solid-web <ozoaniphilip49@gmail.com>
2026-06-22 22:18:46 +0100

    fix: update tawk.to to new APXFund property

---
+++ components/TawkChat.tsx
@@ -0,0 +1,19 @@
+'use client'
+import { useEffect } from 'react'
+
+export default function TawkChat() {
+  useEffect(() => {
+    // Remove any existing tawk scripts first
+    const existing = document.querySelectorAll('script[src*="tawk.to"]')
+    existing.forEach(s => s.remove())
+
+    const s1 = document.createElement('script')
+    const s0 = document.getElementsByTagName('script')[0]
+    s1.async = true
+    s1.src = 'https://embed.tawk.to/6a39a58b452f781d473b5022/1is09r5uj'
+    s1.charset = 'UTF-8'
+    s1.setAttribute('crossorigin', '*')
+    s0.parentNode?.insertBefore(s1, s0)
+  }, [])
+  return null
+}
